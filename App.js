67816142b274aa9e4a1636a3f2cdf28ab3664e17
HamzaShaieb <121043767+HamzaShaieb@users.noreply.github.com>
2023-01-14 16:09:11 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import VideoPlayer  from  './principale src/video_player.js' 
+
+function App() {
+  return (
+   <VideoPlayer/>
+  );
+}
+
+export default App;
